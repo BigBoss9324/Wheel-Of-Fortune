3bf37c070e0b5bcf697deef647d41780a41240b7
BigBoss9324 <ADJordan305@hotmail.com>
2025-08-01 06:51:30 -0500

    client loader

--- src/client/StarterCharacterScripts/Loader.client.lua
+++ src/client/StarterCharacterScripts/Loader.client.lua
@@ -1,1 +1,11 @@
--- local Init = require(game.ReplicatedStorage)
+local RR = game.ReplicatedStorage
+for _, module in ipairs(RR:GetChildren()) do
+	if module:IsA("ModuleScript") then
+		local success, err = pcall(function()
+			return require(module)
+		end)
+		if not success then
+			warn("Failed to load module:", module.Name, "\nError:", err)
+		end
+	end
+end
